3207ff66eb88295867b452118d7482370bd948a6
andri2208 <78659880+andri2208@users.noreply.github.com>
2026-01-18 12:13:13 +0700

    Update 3G_LOGISTICS.py

--- 3G_LOGISTICS.py
+++ 3G_LOGISTICS.py
@@ -15,7 +15,7 @@ st.markdown("""
     <style>
     /* Menghilangkan jarak atas pada aplikasi */
     .block-container {
-        padding-top: 40px;
+        padding-top: 50px;
         padding-bottom: 0rem;
         margin-top: -20px;
     }
@@ -161,6 +161,7 @@ function downloadPDF() {{
                     st.cache_data.clear()
                 except:
                     st.error("Gagal menyimpan data.")
+
 
 
 
