1f4194649c79a877ea2f7e704cc84464ca7e1612
andri2208 <78659880+andri2208@users.noreply.github.com>
2026-01-19 08:50:09 +0700

    Update 3G_LOGISTICS.py

--- 3G_LOGISTICS.py
+++ 3G_LOGISTICS.py
@@ -32,9 +32,9 @@ def get_data():
 st.markdown("""
     <style>
     .stApp { background-color: #FDFCF0; }
-    .block-container { padding-top: 5rem !important; }
+    .block-container { padding-top: 2rem !important; }
     .custom-header { text-align: left; /* PINDAH KE KIRI */ margin-bottom: 20px; }
-    .custom-header img { width: 100%; max-width: 500px; height: auto; border-radius: 8px; }
+    .custom-header img { width: 100%; max-width: 400px; height: auto; border-radius: 8px; }
     .stWidgetLabel p { font-weight: 900 !important; font-size: 14px !important; color: #1A2A3A !important; }
     .stTextInput input {
         background-color: #FFFFFF !important;
@@ -216,6 +216,7 @@ with tab2:
                     st.error(f"❌ GAGAL MENYIMPAN! Status: {r.status_code}")
             except Exception as e:
                 st.error(f"⚠️ Terjadi Kesalahan: {str(e)}")
+
 
 
 
